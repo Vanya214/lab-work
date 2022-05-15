928bd6e3a31a4c41ec5b8dd90d40159fa5eefe58
Ivan <ivan.2000.1974@gmail.com>
2022-05-15 19:44:55 +0300

    Added printlns.

--- src/main/java/labwork/Main.java
+++ src/main/java/labwork/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Branch feature 1");
+        System.out.println("feature 1");
+        System.out.println("feature 1");
+        System.out.println("feature 1");
     }
 }
